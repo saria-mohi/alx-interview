5a80f928fa5d2f5d60e8f7f11836332f41c98434
saria-mohi <70873507+saria-mohi@users.noreply.github.com>
2023-10-26 11:22:08 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
 
+"""Script that reads stdin line by line and computes metrics"""
+
 import sys
 
 
-def print_message(status_code_counts, total_file_size):
-    """
-    Method to print
-    Args:
-        status_code_counts: Dictionary of status codes
-        total_file_size: Total size of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for code, count in sorted(status_code_counts.items()):
-        if count != 0:
-            print("{}: {}".format(code, count))
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-status_code_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
-                      '403': 0, '404': 0, '405': 0, '500': 0}
-total_file_size = 0
-counter = 0
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # Split the line
-        parsed_line = parsed_line[::-1]  # Reverse the order of elements
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-        if len(parsed_line) > 2:
-            counter += 1
+        stlist = line.split()
+        count += 1
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # Extract file size
-                status_code = parsed_line[1]  # Extract status code
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
 
-                if status_code in status_code_counts:
-                    status_code_counts[status_code] += 1
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
 
-            if counter == 10:
-                print_message(status_code_counts, total_file_size)
-                counter = 0
 
-finally:
-    print_message(status_code_counts, total_file_size)
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
